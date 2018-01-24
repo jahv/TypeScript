3b58e3c3e7406f165ab0112593cedd08c98018e0
Jose Antonio Hernandez <ahernandez@nearsoft.com>
2018-01-24 17:37:54 -0600

    const

--- const.ts
+++ const.ts
@@ -1,5 +1,5 @@
 //const foo; //'const' declarations must be initialized.
-const foo = 1;
+const fooX = 1;
 //foo = 2; //Cannot assign to 'foo' because it is a constant or a read-only property.
 
 
@@ -10,3 +10,28 @@ function func() {
       console.log(tmp); //Cannot find name 'tmp'.
   }
 func();
+
+
+/***/
+//Immutable
+let foo = "foo";
+foo = "moo";
+console.log(foo);
+
+const foo1 = 'abc';
+foo1 = 'def'; // Cannot assign to 'foo1' because it is a constant or a read-only property.
+
+//Immutable on objects
+const fooObj = {};
+fooObj = {}; //Cannot assign to 'fooObj' because it is a constant or a read-only property.
+
+const fooObj1 = {};
+fooObj1['prop'] = "Moo"; // Works
+console.log(fooObj1);// { prop: 'Moo' }
+
+
+/**/
+//Avoid immutable on objects
+const fooObj2 = Object.freeze({});
+fooObj2['prop'] = "Moo"; // Works but...
+console.log(fooObj2);//{}
