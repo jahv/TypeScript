44222ab004c135cf28c6d9bd0aa85c3d683d58b6
Jose Antonio Hernandez <ahernandez@nearsoft.com>
2018-01-24 17:56:01 -0600

    template string

---
+++ templateString.ts
@@ -0,0 +1,30 @@
+let single = "Hello World";
+console.log(single);
+
+let multiple = "a " +
+               "b " +
+               "c";
+console.log(multiple);
+
+let block = "a \n" +
+            "b \n" +
+            "c";
+console.log(block);
+
+let backTick = `Hello
+                World
+                my
+                name
+                is`;
+console.log(backTick);
+
+
+/****/
+let name1 = "JAHV";
+
+let backTick1 = `Hello
+World
+my
+name
+is ${name1}`;
+console.log(backTick1);
